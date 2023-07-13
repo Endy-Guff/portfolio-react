448549adae05718dbbd93658dc399b05a9bfb1cb
Endy-Guff <andreymaranchak@yandex.ru>
2023-07-13 23:32:50 +0300

    fix styles

--- src/components/Footer/Item/Item.tsx
+++ src/components/Footer/Item/Item.tsx
@@ -5,12 +5,14 @@ type ItemPropsType = {
     img: string
     alt: string
     link: string
+    children?: React.ReactElement
 }
 
 export const Item = (props: ItemPropsType) => {
     return (
         <a className={s.link} href={props.link}>
-            <img src={props.img} alt={props.alt}/>
+            {props.children}
+            {/*<img src={props.img} alt={props.alt}/>*/}
         </a>
     );
 };
--- src/components/common/ButtonToUp.tsx
+++ src/components/common/ButtonToUp.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import icon from '../../assets/img/btnToUpIcon.svg'
 import s from './ButtonToUp.module.css'
 import {Link} from "react-scroll";
 
@@ -21,7 +20,7 @@ export const ButtonToUp = (props: ButtonToUpPropsType) => {
                     <rect id={s.fill} x="15" y="15" width="60" height="60" rx="30" fill="#2A2B2F"/>
                     <rect x="13.5" y="13.5" width="63" height="63" rx="31.5" stroke="#2A2B2F" stroke-width="3"/>
                 </g>
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M60.5323 53.2143C61.1559 52.5664 61.1559 51.5175 60.5323 50.8713L47.3106 37.1705C46.0618 35.8765 44.036 35.8765 42.7872 37.1705L29.4679 50.9705C28.8507 51.6117 28.8427 52.6476 29.4519 53.2971C30.0739 53.9615 31.0989 53.9678 31.7304 53.315L43.9176 40.6846C44.5428 40.0367 45.555 40.0367 46.1801 40.6846L58.2714 53.2143C58.895 53.8621 59.9087 53.8621 60.5323 53.2143Z" fill="black"/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M60.5323 53.2143C61.1559 52.5664 61.1559 51.5175 60.5323 50.8713L47.3106 37.1705C46.0618 35.8765 44.036 35.8765 42.7872 37.1705L29.4679 50.9705C28.8507 51.6117 28.8427 52.6476 29.4519 53.2971C30.0739 53.9615 31.0989 53.9678 31.7304 53.315L43.9176 40.6846C44.5428 40.0367 45.555 40.0367 46.1801 40.6846L58.2714 53.2143C58.895 53.8621 59.9087 53.8621 60.5323 53.2143Z" fill="#535458"/>
                 <defs>
                     <filter id="filter0_ddii_101_8" x="0" y="0" width="91" height="91" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
                         <feFlood flood-opacity="0" result="BackgroundImageFix"/>

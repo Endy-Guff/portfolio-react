d5d7d31219f9db61ca03c5e9b29e510ad527ad8f
Endy-Guff <andreymaranchak@yandex.ru>
2023-07-15 16:19:11 +0300

    add portfolio item

--- src/components/Main/Portfolio/Portfolio.tsx
+++ src/components/Main/Portfolio/Portfolio.tsx
@@ -5,6 +5,7 @@ import {Item} from "./Item/Item";
 import example from '../../../assets/img/portfolio/example.png'
 import kinoSearch from '../../../assets/img/portfolio/kino-search.jpg'
 import chat from '../../../assets/img/portfolio/chat.jpg'
+import socialNetwork from '../../../assets/img/portfolio/socialNetwork.jpg'
 import {Zoom} from "react-awesome-reveal";
 
 
@@ -16,7 +17,7 @@ export const Portfolio = () => {
                 <Zoom delay={500} triggerOnce>
                 <Item name={'Kino Search'} img={kinoSearch} link={'https://endy-guff.github.io/kino-search-react/'}/>
                 <Item name={'Chat'} img={chat} link={'https://endy-guff.github.io/React-chat-client/'}/>
-                <Item name={'Name'} img={example} link={'#'}/>
+                <Item name={'Social Network'} img={socialNetwork} link={'https://endy-guff.github.io/samurai-way'}/>
                 </Zoom>
             </div>
         </div>
